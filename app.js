70be0862569175399b7a0a70292b17fed7cec85c
Jcmoralesl <juancamilo.morales43@gmail.com>
2024-08-05 22:17:52 -0500

    Configuración para que desaparezca la imagen en el encriptador

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
 
 
 function encriptarDisplay() {
+    //cambio de vocales por encriptado
     textoUsuario = document.getElementById('textoUsuario').value;
     textoCifrado = textoUsuario.replace(/e/gi, "enter").replace(/i/gi, "imes").replace(/a/gi, "ai").replace(/o/gi, "ober").replace(/u/gi, "ufat");
     
+    //deshabilitar la imagen mostrar texto encriptado
+    let imagenOff = document.getElementById('image_1');
+    if (imagenOff.style.display === 'none') {
+        imagenOff.style.display = 'block';
+    } else {
+        imagenOff.style.display = 'none';
+    }
 }
