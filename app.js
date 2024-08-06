ca0357575ccee122e3daed8b19be9342a2e7d073
Jcmoralesl <juancamilo.morales43@gmail.com>
2024-08-05 22:08:15 -0500

    Programa combio de letras a,e,u,i,o por las encriptadas

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
-let textoUsuario = 0;
+
 
 function encriptarDisplay() {
-    textoUsuario = document.getElementById(textoUsuario);
-    console.log(textoUsuario)
+    textoUsuario = document.getElementById('textoUsuario').value;
+    textoCifrado = textoUsuario.replace(/e/gi, "enter").replace(/i/gi, "imes").replace(/a/gi, "ai").replace(/o/gi, "ober").replace(/u/gi, "ufat");
+    
 }
